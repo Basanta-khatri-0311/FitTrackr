87f4479e774f764a59911f0a6fb186c049751876
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-10-06 23:16:23 +0545

    api updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "http://localhost:5500/api", // your backend port
 });
 
+// ===== USER APIs =====
 export const registerUser = (userData) => API.post("/users/register", userData);
 export const loginUser = (userData) => API.post("/users/login", userData);
-export const addWorkout = (workoutData) => API.post("/workouts", workoutData);
+
+// ===== WORKOUT APIs =====
+
+// Fetch all workouts (you can later modify it to use userId or token)
 export const getWorkouts = (userId) => API.get(`/workouts/${userId}`);
+
+// Log (Add) a new workout
+export const logWorkout = (workoutData) => API.post("/workouts", workoutData);
